30bc56cb5400ef02f9804360e64e1bf124aa6c81
ehab-ahma <ehab24495@gmail.com>
2021-09-07 15:21:50 +0300

    fixing lab06

--- src/component/main.js
+++ src/component/main.js
@@ -2,7 +2,9 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import Map from "google-maps-react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -19,13 +21,19 @@ class Main extends React.Component {
   formSubmit = async (e) => {
     e.preventDefault();
     console.log(this.state.cityName);
-    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${this.state.cityName}&format=json`;
+    const url = `GET https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&city=${this.state.cityName}&format=json`;
     console.log(url);
-    const response = await axios.get(url);
-    console.log(response.data[0]);
+    const res = await axios.get(url);
     this.setState({
-      location: response.data[0],
+      location: res.data[0],
     });
+    // console.log(res);
+
+    // const response = await axios.get(url);
+    // console.log(response.data[0]);
+    // this.setState({
+    //   location: response.data[0],
+    // });
   };
 
   render() {
@@ -43,27 +51,41 @@ class Main extends React.Component {
           </Form.Group>
 
           <Button variant="primary" type="submit">
-            Submit
+            Explore!
           </Button>
         </Form>
         <div>
-          <h2>location information</h2>
-          <p>name: {this.state.location.display_name}</p>
-          <p> lat: {this.state.location.lat}</p>
-          <p> lon: {this.state.location.lon}</p>
-        </div>
-        <div>
-          <Map
-            zoom={14}
-            google={this.props.google}
-            initialCenter={{
-              lat: this.state.location.lat,
-              lng: this.state.location.lon,
-            }}
-          ></Map>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                name: {this.state.location.display_name}
+              </ListGroupItem>
+              <ListGroupItem>lat: {this.state.location.lat}</ListGroupItem>
+              <ListGroupItem>lon: {this.state.location.lon}</ListGroupItem>
+            </ListGroup>
+            {/* <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body> */}
+          </Card>
         </div>
       </div>
     );
   }
 }
 export default Main;
+
+// <div>
+//   <h2>location information</h2>
+//   <p>name: {this.state.location.display_name}</p>
+//   <p> lat: {this.state.location.lat}</p>
+//   <p> lon: {this.state.location.lon}</p>
+// </div>
