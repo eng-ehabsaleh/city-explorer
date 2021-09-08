dbd50f8d381b23f33944f3385d7f595f8d3e2e59
ehab-ahma <ehab24495@gmail.com>
2021-09-08 04:54:16 +0300

    adding the alert

---
+++ src/component/Card.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+class Cardy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapurl: "",
+    };
+  }
+
+  map = () => {
+    this.setState({
+      mapurl: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.props.location.lat},${this.props.location.lon}&zoom=18&size=100x180&format=png&markers=icon:large-blue-cutout|${this.props.location.lat},${this.props.location.lon}&markers=icon:large-blue-cutout|${this.props.location.lat},${this.props.location.lon}`,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.state.mapurl} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>
+              name: {this.props.location.display_name}
+            </ListGroupItem>
+            <ListGroupItem>lat: {this.props.location.lat}</ListGroupItem>
+            <ListGroupItem>lon: {this.props.location.lon}</ListGroupItem>
+          </ListGroup>
+          {/* <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body> */}
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default Cardy;
--- src/component/main.js
+++ src/component/main.js
@@ -2,16 +2,17 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
+import Cardy from "../component/Card";
+import Alert from "react-bootstrap/Alert";
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       cityName: "",
       location: {},
-      mapurl: "",
+      showErrMsg: false,
+      showLocation: false,
+      errMsg: "error: Unable to geocode",
     };
   }
   formChange = (e) => {
@@ -21,19 +22,31 @@ class Main extends React.Component {
   };
   formSubmit = async (e) => {
     e.preventDefault();
-    console.log(this.state.cityName);
-    const url = `GET https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&city=${this.state.cityName}&format=json`;
-    console.log(url);
-    const res = await axios.get(url);
-    this.setState({
-      location: res.data[0],
-      mapurl: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=18&size=100x180&format=png&maptype=<MapType>&markers=icon:<icon>|${this.state.location.lat},${this.state.location.lon}&markers=icon:<icon>|${this.state.location.lat},${this.state.location.lon}`,
-    });
+    try {
+      console.log(this.state.cityName);
+      const url = `GET https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&city=${this.state.cityName}&format=json`;
+      console.log(url);
+      const res = await axios.get(url);
+      this.setState({
+        location: res.data[0],
+        showErrMsg: false,
+        showLocation: true,
+      });
+    } catch (err) {
+      this.setState({
+        showErrMsg: true,
+        showLocation: false, //to not desplay the cardy
+        // we caan say errMsg:err.res.data.error|| errMsg:err,message
+      });
+    }
   };
 
   render() {
     return (
       <div>
+        {this.state.showErrMsg && (
+          <Alert variant="dark">{this.state.errMsg}</Alert>
+        )}
         <Form onSubmit={this.formSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>City Name</Form.Label>
@@ -50,23 +63,7 @@ class Main extends React.Component {
           </Button>
         </Form>
         <div>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={this.state.mapurl} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>
-                name: {this.state.location.display_name}
-              </ListGroupItem>
-              <ListGroupItem>lat: {this.state.location.lat}</ListGroupItem>
-              <ListGroupItem>lon: {this.state.location.lon}</ListGroupItem>
-            </ListGroup>
-            {/* <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body> */}
-          </Card>
+          {this.state.showLocation && <Cardy location={this.state.location} />}
         </div>
       </div>
     );
